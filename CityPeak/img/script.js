2ac766969d53edeaad9c00d7397559a1f1951f6b
TandinGyeltshenLepcha <12230089.gcit@rub.edu.bt>
2024-05-22 13:38:10 +0600

    Add files via upload

---
+++ CityPeak/img/script.js
@@ -0,0 +1,7 @@
+function changePassword() {
+    let currentPassword = prompt("What is your current password?");
+    let newpassword= prompt("Set your new password")
+    let confirmpsw=prompt("Confirm your password")
+    alert("Successfully changed the password")
+
+}
